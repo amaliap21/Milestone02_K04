e1561a255ff52295ece3c93c955a7859e6542aaa
ganadipa <nyomanganadipa@gmail.com>
2023-08-01 18:04:18 +0700

    Finish making forms with state than can be easily be customized

--- src/src/Components/Footer.jsx
+++ src/src/Components/Footer.jsx
@@ -1,68 +1,77 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 // import { Switch, Route } from 'react-router-dom'
-import { Router, Route, Link, IndexRoute, hashHistory, browserHistory, DefaultRoute, IndexLink } from 'react-router'
-import '../css/styles.css';
-import { StyleSheet, css } from 'aphrodite/no-important';
+import {
+  Router,
+  Route,
+  Link,
+  IndexRoute,
+  hashHistory,
+  browserHistory,
+  DefaultRoute,
+  IndexLink,
+} from "react-router";
+import "../css/styles.css";
+import { StyleSheet, css } from "aphrodite/no-important";
 
-import RecycleCanLight from '../Images/Recycle_Ontario_Light.png';
-import GitHubLogo from '../Images/GitHub.png';
-
+import RecycleCanLight from "../Images/Recycle_Ontario_Light.png";
+import GitHubLogo from "../Images/GitHub.png";
 
 class Footer extends Component {
   constructor() {
     super();
   }
 
-
   render() {
-
     const styles = StyleSheet.create({
-
       logoLinkStyle: {
-        color:'white',
-        textDecoration:'none',
+        color: "white",
+        textDecoration: "none",
       },
 
       logoImageStyle: {
-
-        width: '240px',
-        height: '80px',
+        width: "240px",
+        height: "80px",
       },
       gitHubLogoStyle: {
-        width: '35px',
-        height: '35px',
+        width: "35px",
+        height: "35px",
       },
-
     });
 
     console.log("Hi, ", this.props.page);
 
     return (
-
       <div id="fixedwidth">
-          <div id="footer">
-              <div id="logodiv2">
-                  <a href='Index.html'>
-                      <p></p>
-                  </a>
+        <div id="footer">
+          <div id="logodiv2">
+            <a href="Index.html">
+              <p></p>
+            </a>
 
-                  <a href='Index.html'><img src={RecycleCanLight} className = {css(styles.logoImageStyle)} /></a>
-                  <p>© Copyright 2017 Recycle Can, inc.</p>
+            <a href="Index.html">
+              <img
+                src={RecycleCanLight}
+                className={css(styles.logoImageStyle)}
+              />
+            </a>
+            <p>© Copyright 2017 Recycle Can, inc.</p>
 
-                      <p><Link to='/terms_privacy'><span style = {{color:'#FFF'}}>Terms of Use and Privacy Policy</span></Link></p>
+            <p>
+              <Link to="/terms_privacy">
+                <span style={{ color: "#FFF" }}>
+                  Terms of Use and Privacy Policy
+                </span>
+              </Link>
+            </p>
 
-                  <a href='https://github.com/EdwaRen/Recycle_Can_Website'><img src={GitHubLogo} className = {css(styles.gitHubLogoStyle)} />
-
-                  </a>
-
-              </div>
+            <a href="https://github.com/EdwaRen/Recycle_Can_Website">
+              <img src={GitHubLogo} className={css(styles.gitHubLogoStyle)} />
+            </a>
           </div>
+        </div>
       </div>
-
     );
   }
-
-
 }
 
 export default Footer;
--- src/src/Components/Paint.jsx
+++ src/src/Components/Paint.jsx
@@ -1,9 +1,9 @@
-import React, {Component} from 'react';
-import { Switch, Route } from 'react-router-dom'
-import {Link} from 'react-router';
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import { Link } from "react-router";
 
-import Header from './Header';
-import Footer from './Footer';
+import Header from "./Header";
+import Footer from "./Footer";
 
 const google = window.google;
 const _ = require("lodash");
@@ -15,52 +15,60 @@ const {
   GoogleMap,
   FusionTablesLayer,
 } = require("react-google-maps");
-const { SearchBox } = require("react-google-maps/lib/components/places/SearchBox");
+const {
+  SearchBox,
+} = require("react-google-maps/lib/components/places/SearchBox");
 
 const MapWithAFusionTablesLayer = compose(
   withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyBipGTaEpZxIOqSRoYJakq64x9BQNjZwJs&v=3.exp&libraries=geometry,drawing,places",
+    googleMapURL:
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyBipGTaEpZxIOqSRoYJakq64x9BQNjZwJs&v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `400px` }} />,
     mapElement: <div style={{ height: `100%` }} />,
   }),
   lifecycle({
     componentWillMount() {
-      const refs = {}
+      const refs = {};
 
       this.setState({
         bounds: null,
         center: {
-          lat: 45.4235937, lng: -75.7031177
+          lat: 45.4235937,
+          lng: -75.7031177,
         },
         markers: [],
-        onMapMounted: ref => {
+        onMapMounted: (ref) => {
           refs.map = ref;
         },
         onBoundsChanged: () => {
           this.setState({
             bounds: refs.map.getBounds(),
             center: refs.map.getCenter(),
-          })
+          });
         },
-        onSearchBoxMounted: ref => {
+        onSearchBoxMounted: (ref) => {
           refs.searchBox = ref;
         },
         onPlacesChanged: () => {
           const places = refs.searchBox.getPlaces();
           const bounds = new google.maps.LatLngBounds();
 
-          places.forEach(place => {
+          places.forEach((place) => {
             if (place.geometry.viewport) {
-              bounds.union(place.geometry.viewport)
+              bounds.union(place.geometry.viewport);
             } else {
-              bounds.extend(place.geometry.location)
+              bounds.extend(place.geometry.location);
             }
           });
-          const nextMarkers = places.map(place => ({
+          const nextMarkers = places.map((place) => ({
             position: place.geometry.location,
           }));
-          const nextCenter = _.get(nextMarkers, '0.position', this.state.center);
+          const nextCenter = _.get(
+            nextMarkers,
+            "0.position",
+            this.state.center
+          );
 
           this.setState({
             center: nextCenter,
@@ -68,87 +76,79 @@ const MapWithAFusionTablesLayer = compose(
           });
           // refs.map.fitBounds(bounds);
         },
-      })
+      });
     },
   }),
   withScriptjs,
   withGoogleMap
-)(props =>
+)((props) => (
   <GoogleMap
     ref={props.onMapMounted}
-
     defaultZoom={11}
     defaultCenter={{ lat: 45.4235937, lng: -75.7031177 }}
     center={props.center}
 
     // onBoundsChanged={props.onBoundsChanged}
-
+  >
+    {/* Paint: 1R3jmt90pLFytzfWEYB2myzsBhkrvwh8JfPn4eImG */}
+    {/* Ewaste: 1ySrhzEk6ubvy7EQZeG7zWEnP4UUNDbhcFviGiHBB */}
+    <FusionTablesLayer
+      url="http://googlemaps.github.io/js-v2-samples/ggeoxml/cta.kml"
+      options={{
+        query: {
+          select: `Name`,
+          from: `1R3jmt90pLFytzfWEYB2myzsBhkrvwh8JfPn4eImG`,
+          // where: `Latitude ==0`
+        },
+      }}
+    />
+    <SearchBox
+      ref={props.onSearchBoxMounted}
+      bounds={props.bounds}
+      controlPosition={google.maps.ControlPosition.TOP_LEFT}
+      onPlacesChanged={props.onPlacesChanged}
     >
-
-      {/* Paint: 1R3jmt90pLFytzfWEYB2myzsBhkrvwh8JfPn4eImG */}
-      {/* Ewaste: 1ySrhzEk6ubvy7EQZeG7zWEnP4UUNDbhcFviGiHBB */}
-      <FusionTablesLayer
-        url="http://googlemaps.github.io/js-v2-samples/ggeoxml/cta.kml"
-        options={{
-          query: {
-            select: `Name`,
-            from: `1R3jmt90pLFytzfWEYB2myzsBhkrvwh8JfPn4eImG`,
-            // where: `Latitude ==0`
-          }
+      <input
+        type="text"
+        placeholder="Search ..."
+        style={{
+          boxSizing: `border-box`,
+          border: `1px solid transparent`,
+          width: `240px`,
+          height: `30px`,
+          marginTop: `9px`,
+          padding: `0 12px`,
+          borderRadius: `3px`,
+          boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+          fontSize: `14px`,
+          outline: `none`,
+          textOverflow: `ellipses`,
         }}
       />
-      <SearchBox
-        ref={props.onSearchBoxMounted}
-        bounds={props.bounds}
-        controlPosition={google.maps.ControlPosition.TOP_LEFT}
-        onPlacesChanged={props.onPlacesChanged}
-        >
-          <input
-            type="text"
-            placeholder="Search ..."
-            style={{
-              boxSizing: `border-box`,
-              border: `1px solid transparent`,
-              width: `240px`,
-              height: `30px`,
-              marginTop: `9px`,
-              padding: `0 12px`,
-              borderRadius: `3px`,
-              boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
-              fontSize: `14px`,
-              outline: `none`,
-              textOverflow: `ellipses`,
-            }}
-          />
-        </SearchBox>
-      </GoogleMap>
-    );
-
+    </SearchBox>
+  </GoogleMap>
+));
 
 class Paint extends Component {
   constructor() {
     super();
   }
   componentDidMount() {
-    window.scrollTo(0,0);
-
+    window.scrollTo(0, 0);
   }
 
   render() {
     const mainDivStyle = {
-      width: '800px',
-      backgroundColor: 'white',
-
-
-    }
+      width: "800px",
+      backgroundColor: "white",
+    };
 
     return (
       <div>
-        <Header page = {3}/>
+        <Header page={3} />
         <section id="headerImgPaint">
           <article>
-            <div id="fixedWidth">
-            </div>
+            <div id="fixedWidth"></div>
           </article>
 
           <div class="parallax" data-velocity="-.3"></div>
@@ -159,45 +159,86 @@ class Paint extends Component {
                   <h1> Recycle Your Paint </h1>
                   <h1> </h1>
                 </div>
-
               </div>
             </div>
           </div>
         </section>
 
-        <div id="overviewE" style = {{height:'900px'}}>
+        <div id="overviewE" style={{ height: "900px" }}>
           <div id="fixedWidth">
             <div id="desclong3">
               <div id="header3">
-                <h1><b>Enter Your Address or Postal Code To Begin</b></h1>
+                <h1>
+                  <b>Enter Your Address or Postal Code To Begin</b>
+                </h1>
               </div>
             </div>
           </div>
 
-
           <p id="demo"></p>
 
-          <div id="fixedWidthM" style = {{width:"100%"}}>
-            <input id="pac-input" class="controls" type="text" placeholder="Search Box" disabled style = {{opacity:'0'}}/>
-            <div style = {{backgroundColor:"#333"}} />
+          <div id="fixedWidthM" style={{ width: "100%" }}>
+            <input
+              id="pac-input"
+              class="controls"
+              type="text"
+              placeholder="Search Box"
+              disabled
+              style={{ opacity: "0" }}
+            />
+            <div style={{ backgroundColor: "#333" }} />
 
             {/* 45.248786,-76.3607093 */}
 
             {/* {this.numberFilesToRender()} */}
-            <MapWithAFusionTablesLayer />
             {/* <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDHlW8YibL2zp7mdBSNeT81AYVaxHuOk3A&libraries=places&callback=initAutocomplete" async defer></script> */}
-
-
           </div>
-
+          <FormConversionRate />
           <Footer />
         </div>
-
       </div>
     );
   }
+}
 
+class FormConversionRate extends Component {
+  state = {
+    organicWeight: "or",
+    metalWeight: "me",
+    plasticWeight: "pl",
+  };
 
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  render() {
+    const { organicWeight, metalWeight, plasticWeight } = this.state;
+    return (
+      <div>
+        <form style={{ display: "flex", flexDirection: "column" }}>
+          <input
+            type="text"
+            value={this.state.organicWeight}
+            onChange={this.onChange}
+            name="organicWeight"
+          />
+          <input
+            type="text"
+            value={this.state.metalWeight}
+            onChange={this.onChange}
+            name="metalWeight"
+          />
+          <input
+            type="text"
+            value={this.state.plasticWeight}
+            onChange={this.onChange}
+            name="plasticWeight"
+          />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Paint;
