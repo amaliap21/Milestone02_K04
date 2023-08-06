a564dfa0771aca5ad6b171d4beb709d873635c0b
InTheRain <indrswara@gmail.com>
2023-08-06 12:24:01 +0700

    resolve problem

--- src/src/Components/App.jsx
+++ src/src/Components/App.jsx
@@ -26,13 +26,12 @@ import article3 from "./TrashGuide/Articles/Article3";
 import article4 from "./TrashGuide/Articles/Article4";
 import article5 from "./TrashGuide/Articles/Article5";
 import article6 from "./TrashGuide/Articles/Article6";
+
+//css 
 import '../css/styles.css';
 import '../css/TrashGuide.css'
 
 
-import test from "../Components/TrashGuide/test";
-import "../css/styles.css";
-import Reward from "./Reward";
 
 // App.jsx is not 'actually necessary' but it follows accepted react architecture by always having an App.jsx main renderer
 class App extends Component {
@@ -60,6 +59,7 @@ class App extends Component {
         <Route path="/Reward" component={Reward} />
         <Route path="/about" component={About} />
         <Route path="/terms_privacy" component={Terms} />
+        {/* route article */}
         <Route path= "/Article1" component={article1} />
         <Route path= "/Article2" component={article2} />
         <Route path= "/Article3" component={article3} />
@@ -69,7 +69,6 @@ class App extends Component {
         <Route path="/reward" component={Reward} />
         <Route path="/about" component={About} />
         <Route path="/terms_privacy" component={Terms} />
-        <Route path="/test" component={test} />
       </Router>
     );
   }
